6572d67ffcbd4104154a0998d72d9959fb65503a
noopy <young_ahn@yonsei.ac.kr>
2020-03-17 13:49:56 +0900

    first compiling pug.js to  ugly HTML

--- README.md
+++ README.md
@@ -1,8 +1,36 @@
+# Gulp-Gulp-Gulp
 
+
+
+Tasks of Gulps
+
+- Pug.js -> ugly HTML
+
+  - https://www.npmjs.com/package/gulp-pug
+
+  - ```shell
+    yarn add gulp-pug -D
+    ```
+
+  - 
+
+- SCSS -> ugly CSS
+
+- New JavaScript -> Old JavaScript
+
+- uploading to github automization
+
+Initializing Gulp
 
 ```shell
 yarn add @babel/{register,core}
 yarn add @babel/preset-env
+```
+
+
+
+```shell
+yarn dev
 ```
 
 
--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,1 +1,19 @@
 import gulp from "gulp";
+import gulpPug from "gulp-pug";
+
+// transfer source file in pug to html file in destination
+const routes = {
+  pug: {
+    src: "src/*.pug",
+    destination: "build"
+  }
+};
+
+export const pug_to_html = () =>
+  gulp
+    .src(routes.pug.src)
+    .pipe(gulpPug())
+    .pipe(gulp.dest(routes.pug.destination));
+
+// results are at ./build
+export const dev = gulp.series([pug_to_html]);
